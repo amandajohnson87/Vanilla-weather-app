a962d6f83ec602c03dfbaa4fdbec86a75e8ed2eb
amandajohnson87 <71295556+amandajohnson87@users.noreply.github.com>
2020-10-20 20:34:48 -0700

    Added forecast API

--- script.js
+++ script.js
@@ -254,6 +254,20 @@ function formatCountry (countryCode) {
     }
 }
 
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+    return `${hours}:${minutes}`;
+}
+
+
 function formatDate(timestamp) {
 let date = new Date(timestamp);
 let days = [
@@ -272,7 +286,7 @@ if (hours < 10) {
 }
 let minutes = date.getMinutes();
 if (minutes < 10) {
-    mintes = `0${minutes}`;
+    minutes = `0${minutes}`;
 }
 
 let day = days[date.getDay()];
@@ -307,6 +321,8 @@ let countryElement = document.querySelector("#country");
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
     
+
+
 }
 
 function handleSubmit(event) {
@@ -319,9 +335,37 @@ function searchCity(city) {
   let apiKey = "961667857ac92e50fc2594f42d82b1ee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
-
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
+}
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
